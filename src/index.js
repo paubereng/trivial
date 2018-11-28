a23f8b7a210cdfa9064cdba83cf914a18aa5e916
paubereng <pauberengueras@gmail.com>
2018-11-28 19:09:50 +0100

    Added code to allow view store data on chrome extension.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Route } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
+//import { Route } from 'react-router';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
+import QuestionsPage from './QuestionsPage';
+import ResultPage from './ResultPage';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+  applyMiddleware(thunk)
+ ));
 
 ReactDOM.render((
-<BrowserRouter>
-  <div>
-    <Route path="/" component={App} />
-  </div>
-</BrowserRouter>
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/questions" component={QuestionsPage} />
+        <Route path="/result" component={ResultPage} />
+      </div>
+    </Router>
+</Provider>
 ), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
