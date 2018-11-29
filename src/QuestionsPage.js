a5f04a0d0656be1e26e8f5095f720745ffe21b9a
paubereng <pauberengueras@gmail.com>
2018-11-29 11:37:53 +0100

    Added transitions, it can improve.

--- src/QuestionsPage.js
+++ src/QuestionsPage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actionsCreators from './reducers/GameOptionsReducer';
+import { CSSTransition, TransitionGroup} from 'react-transition-group';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import QuestionCard from './QuestionCard';
 
 class QuestionsPage extends Component {
@@ -27,7 +29,16 @@ class QuestionsPage extends Component {
       return (
         <div>
             {this.props.options.questions && this.props.options.questions.length
-              ? <QuestionCard nextQuestion={this.nextQuestion} data={this.props.options.questions[currentQuestion]}/>//this.renderQuestionCard(this.props.options.questions[currentQuestion])
+              ?
+              <TransitionGroup className="card-container">
+                  <CSSTransition
+                      key={currentQuestion}
+                      timeout={700}
+                      classNames="slide"
+                  >
+                    <QuestionCard nextQuestion={this.nextQuestion} data={this.props.options.questions[currentQuestion]}/>
+                    </CSSTransition>
+                </TransitionGroup>
               :
                 <div>
                 loading...
