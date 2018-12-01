8e30413d58b91a2fa3f6b8f7b54adf9407bb1393
paubereng <pauberengueras@gmail.com>
2018-12-01 11:30:26 +0100

    Refactored navigation.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-import GameForm from './GameForm';
+
 class App extends Component {
   render() {
     return (
       <div className="App container">
-        <div>
-          <GameForm/>
+        <div className="has-text-centered">
+          <h1 className="title is-1 has-text-light">TRIVIAL REACT</h1>
         </div>
+        {this.props.children}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,28 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-//import { Route } from 'react-router';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 import QuestionsPage from './QuestionsPage';
 import ResultPage from './ResultPage';
+import GameForm from './GameForm';
+import createHistory from 'history/createBrowserHistory';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
   applyMiddleware(thunk)
  ));
+const history = createHistory();
+
 
 ReactDOM.render((
   <Provider store={store}>
-    <Router>
-      <div>
-        <Route exact path="/" component={App} />
-        <Route path="/questions" component={QuestionsPage} />
-        <Route path="/result" component={ResultPage} />
-      </div>
+    <Router history={history}>
+      <App>
+        <div>
+          <Route exact path="/" component={GameForm} />
+          <Route path="/questions" component={QuestionsPage} />
+          <Route path="/result" component={ResultPage} />
+        </div>
+        </App>
     </Router>
 </Provider>
 ), document.getElementById('root'));
